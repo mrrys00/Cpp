7b7d8b78ea113d652560cf2429abe0ffadf0acea
mrrys00 <rysszymon00@gmail.com>
2022-03-18 17:26:09 +0100

    add lab02 todo

---
+++ Lab02/matrix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#include "matrix.h"
+
+#ifndef _MSC_FULL_VER // if not Visual Studio Compiler
+    #warning "Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym"
+#else
+    #pragma message ("Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym")
+#endif
+
