41094edfdfdf61d0aee5a0c70f7662d93b5e8b77
Szymon Ryś <rysszymon00@gmail.com>
2022-04-01 08:37:48 +0200

    add Lab03 template

---
+++ Lab03/fraction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdexcept> // std::out_of_range
+#include <numeric> // std::gcd
+
+#include "fraction.h"
+
+#ifndef _MSC_FULL_VER // if not Visual Studio Compiler
+    #warning "Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym"
+#else
+    #pragma message ("Klasa jest do zaimplementowania. Instrukcja w pliku naglowkowym")
+#endif
+// TODO: zaimplementowac metody klasy, usunac ten komentarz i warning powyzszy
